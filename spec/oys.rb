0b7bc86ba7731ce06807556c54a18805514666f0
riz1702 <rajshree.dehulia@gmail.com>
2019-11-07 16:00:33 +0000

    Failed test for minimum balance method written

--- spec/oys.rb
+++ spec/oys.rb
@@ -1,23 +1,21 @@
 require 'oystercard'
+
 describe OysterCard do
+  let(:oystercard) {OysterCard.new(0)}
  it 'can create an instance of a oystercard' do
-   oystercard = OysterCard.new(0)
    expect(oystercard).to be_an_instance_of(OysterCard)
  end
 
  it 'can check if the oystercard has a balance' do
-   oystercard = OysterCard.new(0)
    expect(oystercard.balance).to eq(0)
  end
 end
 describe 'top_up' do
  it 'can add a new balance and top up the oystercard ' do
-   oystercard = OysterCard.new(0)
    expect{oystercard.top_up(10)}.to change {oystercard.balance}.from(0).to(10)
  end
 
  it 'should raise an error when balance becomes more than £90' do
- oystercard = OysterCard.new(0)
  LIMIT = 90
  expect{oystercard.top_up(100)}.to raise_error "Maximum balance  of #{LIMIT} exceeded"
 end
@@ -45,11 +43,8 @@ describe 'in_journey?' do
     expect{oystercard.touch_out}.to change{oystercard.card_state}.from(true).to(false)
   end
 
-  it 'can check minimum_balance on the card' do
-    oystercard = OysterCard.new(0)
-    expect{oystercard.min_balance(0)}.to raise_error "Insufficient Balance"
-  end
-  end
+  it 'can check minimum balance on touch_in'
+  oystercard = OysterCard.new()
 
-
+  end
 end
